d13c3b93b1c3d838493221d34dfe7471b8ec6058
zinedine <zine.aitaider@yahoo.com>
2024-12-09 14:57:56 +0100

    Configure GitHub Actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react"; // Import du plugin React
 
 export default defineConfig({
+  base: '/scroller/', // Chemin de base pour GitHub Pages
   plugins: [react()], // Ajout du plugin à la configuration
 });
